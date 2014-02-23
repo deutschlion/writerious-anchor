14065e7816a51aa22535fbe5c87a09914c7ccd6f
stefan machovec <mail@stfn.cz>
2014-02-23 18:33:31 +0100

    update

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<div id="footer">
 			<div class="copyright">&copy; <a href="<?php echo base_url(); ?>" title="<?php echo site_name(); ?> [go to homepage]"><?php echo site_name(); ?></a> 2013</div>
-			<div class="theme-info">Powered by <a href="http://anchorcms.com" rel="nofollow">Anchor</a> and   <!-- DO NOT MODIFY OR DELETE THIS LINK --><a href="http://themes.axf.cz" rel="nofollow">Writerious theme</a><!-- DO NOT MODIFY OR DELETE THIS LINK --></div>
+			<div class="theme-info">Powered by <a href="http://anchorcms.com" rel="nofollow">Anchor</a> and   <!-- DO NOT MODIFY OR DELETE THIS LINK --><a href="http://stfn.cz" rel="nofollow">Writerious theme</a><!-- DO NOT MODIFY OR DELETE THIS LINK --></div>
 		</div><!-- \footer -->
 
 	</div><!-- \wrapper -->	
